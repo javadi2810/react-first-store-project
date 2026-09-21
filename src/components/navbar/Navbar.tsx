b2f7ff7b631ba26d26bb0d4417f7fcce0237cece
mohammadkhaliljavdi <mohammadkhaliljavadi@gmail.com>
2026-09-21 09:32:04 +0330

    add linck my  To Do List

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,12 +6,15 @@ function Navbar() {
         <div className="h-20 border-b   bg-pink-300  shadow-xl  flex justify-between items-center"  >
             <Container className='mx-auto'>
                 <div className='flex justify-between items-center '>
-                    <ul className='flex gap-2'>
+                    <ul className='flex gap-4'>
                         <li>
                             <Link to="/" >خانه</Link>
                         </li>
                         <li>
                             <Link to="/Store" >فروشگاه</Link>
+                        </li>
+                        <li>
+                            <a href="https://the-to-do-list-5z9e.vercel.app/">فهرست کارها</a>
                         </li>
                     
                     </ul>
